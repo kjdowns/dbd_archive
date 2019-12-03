ba3ffe975c7dc123c5eceeebdb05b5049586fd31
Kevin Downs <kevin.downs04@gmail.com>
2019-12-03 09:15:21 +0000

    started implementing scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,4 +7,29 @@ class DBDArchive::Scraper
     @doc = Nokogiri::HTML(open("https://deadbydaylight.gamepedia.com/Dead_by_Daylight_Wiki"))
   end
   
+  def scrape_surv_list
+    name_array = []
+    self.doc.css("#fpsurvivors div.fplinks div.link").each{|surv| name_array << surv.text}
+    name_array
+  end
+  
 end
+
+
+# surv list => doc.css("#fpsurvivors div.fplinks div.link") ~~name => list[i].text
+# links for pages => https://deadbydaylight.gamepedia.com/#{name.gsub(" ", "_")}
+# killer list => doc.css("#fpkiller div.fplinks div.link") ~~name => list[i].text ~link list[i]
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
